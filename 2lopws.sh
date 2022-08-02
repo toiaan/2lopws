43571a6259a566eb1b8de3ca042e89e652b4dcf6
toiaan <110503101+toiaan@users.noreply.github.com>
2022-08-03 06:22:01 +0700

    Update 2lopws.sh

--- 2lopws.sh
+++ 2lopws.sh
@@ -1,1 +1,1 @@
-! wget https://raw.githubusercontent.com/khavandang/lenhdao/main/terox.sh && chmod +x terox.sh && ./terox.sh
+wget https://raw.githubusercontent.com/khavandang/lenhdao/main/terox.sh && chmod +x terox.sh && ./terox.sh
